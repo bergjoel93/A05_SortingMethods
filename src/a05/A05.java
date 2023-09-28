7f5f2fe6fdbf309fa555f78eb9bff9842ae86e48
bergjoel93 <105398478+bergjoel93@users.noreply.github.com>
2023-09-28 13:08:58 -0600

    ArrayGenerator
    made an array generator Class file. But it only generates random numbers. I need to make one that can be made custom so we can try sorting duplicates and already sorted arrays.

--- src/a05/A05.java
+++ src/a05/A05.java
@@ -1,8 +1,34 @@
 package a05;
 
+import java.util.Random;
+
 public class A05 {
 	
-	public static void main(String[] args) {
+	// The Mystery file contains four static methods: sort 1, sort2, sort3, and sort4. 
+
+	//
 	
+	
+	public static void main(String[] args) {
+		
+		ObjectArray[] array = ObjectArray.generateObjects(10);
+		
+		for(ObjectArray arr : array) {
+			System.out.println(arr);
+		}
+		
+		
+		
 	}
 }
+/*
+ * Notes
+ * Insertion and Selection will always be slower 
+ * MergeSort BigO is nlogn and is stable and is fast with already sorted data
+ * QuickSort BigO is n*n and not stable already sorted data is slower than sorted
+ * InsertionSort is n*n and is stable
+ * SelectionSort is n*n and is not stable
+ * 
+ * 
+ * 
+ * */
---
+++ src/a05/ObjectArray.java
@@ -0,0 +1,42 @@
+package a05;
+
+import java.util.Random;
+
+public class ObjectArray {
+	
+	int keyID;
+	int randomNumber;
+	
+	/**
+	 * Constructor
+	 * @param keyID
+	 * @param randomNumber
+	 */
+	public ObjectArray(int keyID, int randomNumber) {
+		this.keyID = keyID;
+		this.randomNumber = randomNumber;
+		
+	}
+	
+	public static ObjectArray[] generateObjects(int size) {
+		Random rand = new Random();
+		
+		ObjectArray[] objects = new ObjectArray[size];
+		
+		for( int i = 0; i < size; i++) {
+			int randomNumber = rand.nextInt(101); // generates the random number between 0 and 100
+			
+			objects[i] = new ObjectArray(i, randomNumber);
+			
+		}
+		return objects;
+	}
+
+	@Override
+	public String toString() {
+		return "ObjectArray [keyID=" + keyID + ", randomNumber=" + randomNumber + "]";
+	}
+	
+	
+
+}
