1c39d162f2453d1bd7d57c001410553b2fffa3ad
bergjoel93 <105398478+bergjoel93@users.noreply.github.com>
2023-09-30 13:35:10 -0600

    update

--- src/a05/ObjectArray.java
+++ src/a05/ObjectArray.java
@@ -1,24 +1,54 @@
 package a05;
 
 import java.util.Random;
-
+/**
+ * Is an array that has elements consisting of two attributes: a keyID, and a number.
+ * 
+ */
 public class ObjectArray implements Comparable<ObjectArray>{
 	
 	int keyID;
-	int randomNumber;
+	int number;
+	private static int keyIDCounter = 0;
+	
 	
 	/**
 	 * Constructor
 	 * @param keyID
 	 * @param randomNumber
 	 */
-	public ObjectArray(int keyID, int randomNumber) {
-		this.keyID = keyID;
-		this.randomNumber = randomNumber;
-		
+	public ObjectArray(int number) {
+		this.number = number;
+		this.keyID = keyIDCounter++;
+	}
+	/**
+	 * returns keyID
+	 * @return
+	 */
+	public int getKeyID() {
+		return keyID;
 	}
 	
-	public static ObjectArray[] generateObjects(int size) {
+	public int getNumber() {
+		return number;
+	}
+	
+	public static ObjectArray[] createArray(int[] values) {
+		ObjectArray[] objectArray = new ObjectArray[values.length];
+		for(int i = 0; i < values.length; i++) {
+			objectArray[i] = new ObjectArray(values[i]);
+		}
+		return objectArray;
+	}
+	
+	private static int generateRandomNumber() {
+		// insert random number generator logic here. 
+		
+		Random rand = new Random();
+		return rand.nextInt(101); // generates a random number from 0 to 100. 
+	}
+	
+	/*public static ObjectArray[] generateObjects(int size) {
 		Random rand = new Random();
 		
 		ObjectArray[] objects = new ObjectArray[size];
@@ -26,19 +56,22 @@ public class ObjectArray implements Comparable<ObjectArray>{
 		for( int i = 0; i < size; i++) {
 			int randomNumber = rand.nextInt(101); // generates the random number between 0 and 100
 			
-			objects[i] = new ObjectArray(i, randomNumber);
+			objects[i] = new ObjectArray(number);
 			
 		}
 		return objects;
 	}
+	*/
+	
+	
 	 @Override
 	    public int compareTo(ObjectArray other) {
-	        return Integer.compare(this.randomNumber, other.randomNumber);
+	        return Integer.compare(this.number, other.number);
 	    }
 
 	@Override
 	public String toString() {
-		return "ObjectArray [keyID=" + keyID + ", randomNumber=" + randomNumber + "]";
+		return "ObjectArray [keyID=" + keyID + ", number=" + number + "]";
 	}
 
 }
